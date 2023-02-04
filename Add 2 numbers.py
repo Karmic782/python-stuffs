84ab629e581258b3ad20f96f16409f0efb3d697d
Karmic782 <109705007+Karmic782@users.noreply.github.com>
2023-02-04 16:27:16 -0500

    it works!
    the thing made prices

---
+++ Add 2 numbers.py
@@ -0,0 +1,5 @@
+#this is a program to get 2 numbers and add them together
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+numtotal = num1 + num2
+print(numtotal)
